116cf796334da38b65aa7bf290b86a09cecc4097
eljid-oussama <129512321+eljid-oussama@users.noreply.github.com>
2023-05-14 19:38:35 +0200

    termainison de mictcp-v4.1 , il reste a la tester

--- src/MICTCP-v4_1.c
+++ src/MICTCP-v4_1.c
@@ -8,6 +8,7 @@ mic_tcp_sock_addr sock_addr;
 int PA = 0;
 int PE = 0;
 int perte_tolere = 0 ;
+int perte_final = 0 ;
 int tab[TAILLE] = {[0 ... TAILLE-1] = 1};
 int indice_prochain_mesg = 0;
 
@@ -94,7 +95,7 @@ int mic_tcp_accept(int socket, mic_tcp_sock_addr* addr)
             pthread_cond_wait(&condx, &mut);
     }
     
-    if(pthread_mutex_unlock(&mutrx)!= 0){/*traitement termine*/
+    if(pthread_mutex_unlock(&mutrx)!= 0){/*traitement termine et deverouillage du mutex*/
         printf("erruer de deverouillage")
     }
     result = 0;
@@ -112,15 +113,51 @@ int mic_tcp_connect(int socket, mic_tcp_sock_addr addr)
    mic_tcp_pdu SYN_ACK;
    int SYN_ACK_recv = 0;
    int pourcentage_perte_s;
-   
-
-   //construction du PDU SYN
-   mic_tcp_pdu  SYN = {{sock.addr.port, addr->port, 0, perte_tolere,1,1,0,0}, {"",0}};
+   unsigned long timer = 200;
    
   printf("[MIC-TCP] Appel de la fonction: ");  printf(__FUNCTION__); printf("\n");
   if(socket == sock.fd){
-    sock.state = CONNECTED;
-    sock_addr = addr;
+
+    //construction du PDU SYN
+    mic_tcp_pdu  SYN = {{sock.addr.port, addr.port, 0, 0,0,1,0,0}, {"",0}};
+    while(!SYN_ACK_recv){
+
+      //Envoi de la la demande de connexion SYN
+      if(IP_send(SYN, addr) == -1){
+        printf("erreur de l'envoi de la demande");
+      }
+
+      //attente de l'acquitement SYN_ACK
+      sock.state = WAIT_ACK ;
+      /*allocation de la memoire pour le stockage de SYN_ACK*/
+      SYN_ACK.payload.data = malloc(2*sizeof(short)+2*sizeof(int)+3*sizeof(char));
+
+      if(IP_recv(&SYN_ACK,&sock_addr,timer)!=-1){
+        //on a bien recu un PDU
+        //verification qu'il s'agit bien d'un SYN_ACK
+        if(SYN_ACK.header.ack ==1 && SYN_ACK.header.syn ==1){
+          SYN_ACK_recv = 1 ;
+          pourcentage_perte_s = SYN_ACK.header.ack_num; /*on a stocke le pourcentage de perte propose par le serveur! */
+          
+          /*phase de negociation de %de perte tolere par le client et le serveur*/
+          /*le plus petit % de perte sera aborde*/
+          if(pourcentage_perte_s < perte_tolere){
+            perte_final = pourcentage_perte_s
+          }else{
+            perte_final = perte_tolere
+          }
+          /*envoi d'un acquitement ACK*/
+          mic_tcp_pdu ACK = {{sock.addr.port, addr.port, 0, perte_final,0,1,0},{"",0}};
+           /*envoi de l'acquitement */
+           if(IP_send(ACK, addr) == -1){
+            printf("erreur de l'envoi de l'acquitement ACK");
+           }
+           sock.state = CONNECTED;
+        }
+
+      }
+    }
+    
     result = 0;
   }
   return result;
@@ -135,6 +172,7 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
   int sentsize = -1 ;
   mic_tcp_pdu PDU;
   mic_tcp_pdu ACK;
+  
 
   
    printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
@@ -163,13 +201,13 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
     ACK.payload.size = 2*sizeof(short)+2*sizeof(int)+3*sizeof(char);
     ACK.payload.data = malloc(ACK.payload.size);
 
-    unsigned long timeout =100;
+    unsigned long timer =100;
     int controle = 0 ;
     int result = -1 ;
     //on utilise la boucle puisque les pertes peuvent se produire plus d'une fois
     while(controle == 0){
         /*on a bien recu l'ack correspondant au pdu envoye*/
-        if (((result=IP_recv(&(ACK),&sock_addr, timeout)) != -1) && (ACK.header.ack == 1) && (ACK.header.ack_num == PE)) { 
+        if (((result=IP_recv(&(ACK),&sock_addr, timer)) != -1) && (ACK.header.ack == 1) && (ACK.header.ack_num == PE)) { 
             controle = 1;
            /* Mise a jour de la fenetre d'analyse des pertes */
            tab[indice_prochain_mesg] = 1;
@@ -182,7 +220,7 @@ int mic_tcp_send (int mic_sock, char* mesg, int mesg_size)
         for (int i=0; i<TAILLE; i++) {
             nb_mesg_recus += tab[i];
         } 
-        if(((float)(TAILLE-nb_mesg_recus)/(float)TAILLE*100.0) <= perte_tolere){
+        if(((float)(TAILLE-nb_mesg_recus)/(float)TAILLE*100.0) <= perte_final){
            /*on envoie pas le PDU*/
             controle = 1;
             /*Incrementation de PE*/
@@ -254,8 +292,19 @@ void process_received_PDU(mic_tcp_pdu pdu, mic_tcp_sock_addr addr)
 {        
     mic_tcp_pdu ACK ;
     printf("[MIC-TCP] Appel de la fonction: "); printf(__FUNCTION__); printf("\n");
+    
+    /*on teste le cas de reception d'une demande de connexion*/
+    if(sock.state == WAIT_SYN && pdu.header.syn == 1){
+      /*lancement des threads qui sont en attente sur cond*/
+      int f= pthread_cond_broadcast(&condx);
+      /*deverouillage du mutex*/
+      if(pthread_mutex_unlock(&mutrx)== -1 ){
+        printf("erreur de deverouillage du mutex");
+      }
+    }
+    /*passant au cas d'une pdu mic_tcp*/
     // on teste le n°seq pour accepter la DU
-    if (pdu.header.seq_num == PA) { 
+    else (pdu.header.seq_num == PA) { 
         app_buffer_put(pdu.payload);
         
         // Incrémentation de PA 
